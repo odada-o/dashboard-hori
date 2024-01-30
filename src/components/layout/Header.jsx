d89d5ce9181e4ac81d7e057ff45da7c2a695f729
odd:odd <eehd80yul@gmail.com>
2024-01-30 13:16:08 +0900

    chakra ui 적용

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,29 +1,34 @@
+import { Box, Container } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
-        <header>
-            <h1>
-                <a href="/">Dashboard</a>
-            </h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/">Main Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="/marketplace">NFT Marketplace</a>
-                    </li>
-                    <li>
-                        <a href="/datatables">Data Tables</a>
-                    </li>
-                    <li>
-                        <a href="/profile">Profile</a>
-                    </li>
-                    <li>
-                        <a href="/signin">Sign In</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <Box as="header" borderBottom={'3px solid'} borderColor={'red'}>
+            <Container display={['block', null, 'flex']} justifyContent="space-between">
+                <h1 style={{ width: '50px', height: '100%' }}>
+                    <a href="/">Dashboard</a>
+                </h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Main Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="/marketplace">NFT Marketplace</Link>
+                        </li>
+                        <li>
+                            <Link to="/datatables">Data Tables</Link>
+                        </li>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/signin">Sign In</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </Container>
+        </Box>
     )
 }
 
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,16 +1,17 @@
 import Footer from './Footer'
 import Header from './Header'
+import { Container } from '@chakra-ui/react'
 
 const Layout = (props) => {
     return (
-        <div className="wrap">
+        <>
             <Header />
             <main id="main">
                 <h2>{props.title}</h2>
                 {props.children}
             </main>
             <Footer />
-        </div>
+        </>
     )
 }
 
